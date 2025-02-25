242b98f8cfc09e564528411c484b01a5f82ff941
Jeb Boone <boo21006@byui.edu>
2025-02-25 09:49:28 -0700

    added imput line

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,9 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+        Console.WriteLine("hello Cse 210");
+        Console.WriteLine("please enter your name");
+        string name = Console.ReadLine();
+        Console.WriteLine($"{name}");
     }
 }
