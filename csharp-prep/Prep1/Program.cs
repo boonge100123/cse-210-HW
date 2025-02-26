47377baab3ffe4d8122d98ec6957063c718f5b7c
Jeb Boone <boo21006@byui.edu>
2025-02-26 09:34:59 -0700

    added imputs and print them

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,11 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your First Name?");
+        string firstName = Console.ReadLine();
+        Console.WriteLine("What is your Last Name?");
+        string lastName = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}");
     }
 }
