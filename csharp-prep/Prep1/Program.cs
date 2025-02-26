353ebec41b2cf2b4a488b380227df98a9164a88f
Jeb Boone <boo21006@byui.edu>
2025-02-26 09:43:10 -0700

    specifid file type

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,15 +1,21 @@
+// C#
+
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
+        //! ask for first name and store it in a variable
         Console.WriteLine("Hello Prep1 World!");
         Console.WriteLine("What is your First Name?");
         string firstName = Console.ReadLine();
+
+        //! ask for last name and store it in a variable
         Console.WriteLine("What is your Last Name?");
         string lastName = Console.ReadLine();
 
+    //! print the full name in the format "last name, first name last name"
         Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}");
     }
 }
